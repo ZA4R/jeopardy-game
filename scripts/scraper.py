cb97d43b4dac9e075160eaaaec28f1d1fa661bfa
Duncan Rhoads <97579025+ZA4R@users.noreply.github.com>
2025-06-12 22:52:23 -0500

    scraper complete

--- scripts/scraper.py
+++ scripts/scraper.py
@@ -1,5 +1,19 @@
 import os
 import sys
+import logging
+from bs4 import BeautifulSoup, Tag
+import requests
+import sqlite3
+
+#Traceback (most recent call last):
+#  File "C:\Users\rhoad\Repositories\jeopardy-game\scripts\scraper.py", line 195, in <module>       
+#    main()
+#  File "C:\Users\rhoad\Repositories\jeopardy-game\scripts\scraper.py", line 43, in main
+#    add_jquestion(conn, q)
+#  File "C:\Users\rhoad\Repositories\jeopardy-game\scripts\scraper.py", line 192, in add_jquestion  
+#    cursor.execute("INSERT INTO question (clue, answer, value, category, origin) VALUES (?,?,?,?,?)", (f"{question.clue}",f"{question.answer}",f"{question.value}",f"{question.category}",f"{question.origin}"))
+#    ^^^^^^^^^^^^^^
+#AttributeError: 'builtin_function_or_method' object has no attribute 'execute'
 
 
 #path setup for modules
@@ -7,10 +21,6 @@ current_script_dir = os.path.dirname(os.path.abspath(__file__))
 project_root = os.path.abspath(os.path.join(current_script_dir, os.pardir))
 sys.path.append(project_root)
 
-import logging
-from bs4 import BeautifulSoup, Tag
-import requests
-import sqlite3
 from src.game_utils import Question
 
 # logger setup
@@ -18,40 +28,94 @@ logger = logging.getLogger(__name__)
 
 def main():
     # path setup
-    #script_dir = os.path.dirname(os.path.abspath(__file__))
-    #parent_dir = os.path.dirname(script_dir)
-    #db_path = os.path.join(parent_dir, "data", "questions.db")
-#
-    #try:
-    #    conn = sqlite3.connect(db_path) 
-    #    logger.info(f"Successfully connected to database: {db_path}")
-    #except sqlite3.Error as e:
-    #    logger.error(f"Couldn't connect to or operate on database at '{db_path}': {e}")
-    #except Exception as e:
-    #    logger.error(f"An unexpected error occurred: {e}")
-#
-    #create_table(conn)
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    parent_dir = os.path.dirname(script_dir)
+    db_path = os.path.join(parent_dir, "data", "questions.db")
 
-    search_page_for_categories("https://j-archive.com/showgame.php?game_id=9200")
+    try:
+        conn = sqlite3.connect(db_path) 
+        logger.info(f"Successfully connected to database: {db_path}")
+    except sqlite3.Error as e:
+        logger.error(f"Couldn't connect to or operate on database at '{db_path}': {e}")
+    except Exception as e:
+        logger.error(f"An unexpected error occurred: {e}")
+
+    create_table(conn)
+
+    # gather questions
+    questions: list[Question] = []
+    for i in range(9200):
+        if i == 0: i = 1
+        questions += search_page(f"https://j-archive.com/showgame.php?game_id={i}")
+
+    # add questions to db
+    for q in questions:
+        add_jquestion(conn, q)
     
-    #if conn:
-    #    conn.close()
+    if conn:
+        conn.close()
 
+    print("STATUS: COMPLETE")
+    print("--------------------")
+    print(f"QUESTIONS LOADED: {len(questions)}")
+    print(f"PERCENTAGE OF GAMES USED: {(len(questions) / (9200 * 61.0)) * 100}%")
 
-# J! Archive example page: "https://j-archive.com/showgame.php?game_id=9200"
-# max id 9200-10000
+def search_page(url) -> list[Question]:
+    categories = search_page_for_categories(url)
+    content = search_page_for_questions(url)
+
+    if len(categories) != 13 or len(content) != 61: 
+        logger.error(f"Couldn't scrape sufficient data from {url},\ncontent len = {len(content)}")
+        return []
+
+    questions: list[Question] = []
+    
+    for i in range(12):
+        # loops through all 12 first and second round categories
+        category = categories[i]
+        for j in range(5):
+            # loops through each question per category
+            # $ value can be determined from j
+            value = (j * 100) + 100
+
+            # determine content index with formula
+            if i < 6:
+                clue = content[i + (j*6)][0]
+                answer = content[i + (j*6)][1]
+            else:
+                clue = content[i + 24 + (j*6)][0]
+                answer = content[i + 24 + (j*6)][1]
+
+            # add Questino obj to list to later be added to db
+            questions.append(Question(clue,answer,value,category,"J! Archive"))
+    # Final Jeopardy
+    # hardcoded values work fine as list lengths already determined
+    questions.append(Question(content[60][0],content[60][1],-1,categories[12],"J! Archive"))
+    return questions
+
 def search_page_for_questions(url):
+    """Searches jeopardy url for question/answer pairs.
+
+    Args:
+        url (str): 
+            url of J! Archive webparge, example: "https://j-archive.com/showgame.php?game_id=9200"
+            max id somewhere in 9200-10000
+
+    Returns:
+        list[(question,answer)]: list containing question,answer tuples from webpage
+    """
     try:
         response = requests.get(url)
         response.raise_for_status() 
     except requests.exceptions.RequestException as e:
-        print(f"Error fetching URL: {e}")
+        logger.error(f"Error fetching URL: {e}")
         return []
 
     soup = BeautifulSoup(response.text, "html.parser")
 
-    answers_and_clues = soup.find_all("td", class_="clue_text")
+    answer_question_pairs = []
     
+    answers_and_clues = soup.find_all("td", class_="clue_text")
     for ele in answers_and_clues:
         # skip non-Tag elements
         if not isinstance(ele, Tag):
@@ -62,36 +126,43 @@ def search_page_for_questions(url):
             continue
 
         question = ele.get_text(strip=True)
-        answer = ele.find_next_sibling()
-        # work on naming and flow
-        if answer and isinstance(answer, Tag):
-            answer = answer.find('em', class_='correct_response')
-        if answer:
-            answer = answer.get_text(strip=True)
 
+        # finds specific answer str text, can have appended text without
+        answer_td = ele.find_next_sibling()
+        if answer_td and isinstance(answer_td, Tag):
+            answer_em = answer_td.find('em', class_='correct_response')
+        if answer_em:
+            answer = answer_em.get_text(strip=True)
+
+        if answer and question:
+            answer_question_pairs.append((question,answer))
+            
+    return answer_question_pairs
+    
 def search_page_for_categories(url):
     try:
         response = requests.get(url)
         response.raise_for_status() 
     except requests.exceptions.RequestException as e:
-        print(f"Error fetching URL: {e}")
+        logger.error(f"Error fetching URL: {e}")
         return []
 
     soup = BeautifulSoup(response.text, "html.parser")
 
-    categories = soup.find_all("td", class_="category_name")
+    categories: list[str] = []
+
+    categories_td = soup.find_all("td", class_="category_name")
+
     
-    for ele in categories:
+    for ele in categories_td:
         # skip non-Tag elements
         if not isinstance(ele, Tag):
             continue
 
 
         category = ele.get_text(strip=True)
-        print(category)
-        
-
-    
+        categories.append(category)
+    return categories    
 
 def create_table(conn):
     """Creates necessary table if it don't exist."""
@@ -112,10 +183,9 @@ def create_table(conn):
     except sqlite3 as e:
         logger.error(f"Table couldn't be created: {e}")
     except Exception as e:
-        logger.error(f"Table couldn't be created: {e}")
-    
+        logger.error(f"Table couldn't be created: {e}")    
 
-def add_jquestion(conn,clue,answer,value,category,origin):
+def add_jquestion(conn, question: Question):
     """Adds jeopardy question to question db
 
     Args:
@@ -126,10 +196,10 @@ def add_jquestion(conn,clue,answer,value,category,origin):
         category (str)
         origin (str)
     """
-    cursor = conn.cursor
+    cursor = conn.cursor()
 
     with conn:
-        cursor.execute("INSERT INTO question (clue, answer, value, category, origin) VALUES (?,?,?,?,?)", (f"{clue}",f"{answer}",f"{value}",f"{category}",f"{origin}"))
+        cursor.execute("INSERT INTO questions (clue, answer, value, category, origin) VALUES (?,?,?,?,?)", (f"{question.clue}",f"{question.answer}",f"{question.value}",f"{question.category}",f"{question.origin}"))
     
 if __name__ == "__main__":
     main()
